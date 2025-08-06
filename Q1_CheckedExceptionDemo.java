7063fac87ea65d1a5d76b76303081a27c3f9187d
yashbhardwaj759 <lakshyabhardwaj0070@gmail.com>
2025-08-06 11:13:47 +0530

    cluster 2 sec Q

---
+++ Q1_CheckedExceptionDemo.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+
+public class Q1_CheckedExceptionDemo {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("data.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+}
