4f7e5fda5d00685744e648662d23273f00572bbd
yashbhardwaj759 <lakshyabhardwaj0070@gmail.com>
2025-08-06 11:14:15 +0530

    cluster 2 sec Q

---
+++ Q2_UncheckedExceptionDemo.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class Q2_UncheckedExceptionDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Enter numerator: ");
+            int a = sc.nextInt();
+            System.out.print("Enter denominator: ");
+            int b = sc.nextInt();
+            System.out.println("Result: " + (a / b));
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero!");
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input! Please enter numeric values.");
+        }
+    }
+}
